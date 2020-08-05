1a2cf77068406c0812dab12e421dea66e4f1705e
Haris Rahman <haris.rahman@larisin.id>
2020-08-05 14:06:26 +0700

    [INIT PROJECT] Add layout components

---
+++ src/components/Content.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Content(props) {
+  return (
+    <>
+      <div className='container'>
+        <div className='columns is-multiline'>
+          <div className='column is-full'>content</div>
+        </div>
+      </div>
+    </>
+  );
+}
