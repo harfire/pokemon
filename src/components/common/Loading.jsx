bc34368763a1eb97f7058d3d5ab525aec629bf7d
Haris Rahman <haris.rahman@larisin.id>
2020-08-06 14:29:56 +0700

    [CONTENT] Create common component loading

---
+++ src/components/common/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <>
+      <span>Loading... </span>
+      <span className='icon has-text-grey'>
+        <i className='fas fa-spinner fa-pulse'></i>
+      </span>
+    </>
+  );
+}
