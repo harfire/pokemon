a88e4f3fff9d049df1126201cc709477ff01a1ba
Haris Rahman <haris.rahman@larisin.id>
2020-08-06 14:32:21 +0700

    Add helpers function to get pokemon id from string URL

---
+++ src/helpers/constructImageUrl.js
@@ -0,0 +1,14 @@
+export default function constructImageUrl(url) {
+  if (url) {
+    let pokemonImgUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+    const split = url
+      .split('/')
+      .filter((val) => Number(val))
+      .toString();
+
+    return `${pokemonImgUrl}${split}.png`;
+  }
+
+  return '';
+}
