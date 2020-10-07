4fb19b5c3a0e284e7cee17d5f70bf57742494b7d
Haris Rahman <harisrahman@Hariss-MacBook-Pro.local>
2020-10-07 11:20:47 +0700

    Remove unused testing code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,14 @@ import React from 'react';
 // import classes from '../assets/sass/modules/header.module.scss';
 
 export default function Header() {
-	return (
-		<div className="columns is-gapless is-multiline">
-			<div className="column is-one-fifth">&nbsp;</div>
-			<div className="column">
-				<h1 className="is-size-2 has-text-centered is-family-monospace">Pokédex</h1>
-				{/* <h1 className={classes.headerColor}>Pokédex</h1> */}
-			</div>
-			<div className="column is-one-fifth has-text-right">
-				<div className="buttons">
-					<button type="button" className="button is-info">
-						Info
-					</button>
-					<button type="button" className="button is-success">
-						Success
-					</button>
-					<button type="button" className="button is-warning">
-						Warning
-					</button>
-					<button type="button" className="button is-danger">
-						Danger
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="columns is-gapless is-multiline">
+      <div className="column is-one-fifth">&nbsp;</div>
+      <div className="column">
+        <h1 className="is-size-2 has-text-centered is-family-monospace">Pokédex</h1>
+        {/* <h1 className={classes.headerColor}>Pokédex</h1> */}
+      </div>
+      <div className="column is-one-fifth has-text-right">&nbsp;</div>
+    </div>
+  );
 }
